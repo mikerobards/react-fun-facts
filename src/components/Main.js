2188b71a5feb39097bd0915acb62b84b6d000cda
Michael Robards <mikerobards@icloud.com>
2021-11-26 13:29:47 -0600

    style main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,8 @@
 export default function Main() {
     return (
-        <div>
-            <h1>Main Content goes here</h1>
-            <h1>Fun facts about React</h1>
-            <ul>
+        <main className="main">
+            <h1 className="main-title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on Github</li>
@@ -11,6 +10,6 @@ export default function Main() {
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
 
-        </div>
+        </main>
     )
 }
