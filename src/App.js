26eb55f742cd06ede2d72e6320262de2086ab5a7
Michael Robards <mikerobards@icloud.com>
2021-11-26 12:18:42 -0600

    style navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react"
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
-import logo from './react-logo.png'
 
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,13 @@
+import React from 'react'
+import logo from '../img/react-logo.png'
+
+
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img src={logo} alt="react logo" className="logo" />
+            <h3 className="logo-title">ReactFacts</h3>
+            <h4 className="project-title">React Course - Project 1</h4>
+        </nav>
     )
 }
