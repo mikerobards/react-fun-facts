7ad31e19cd386a3fa544b2d5dc703817ce96f8d9
Michael Robards <mikerobards@icloud.com>
2021-11-26 11:20:19 -0600

    project setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react"
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 import logo from './react-logo.png'
 
 
 function App() {
   return (
-    <div>
-      <img className="logo" src={logo} alt="react logo" />
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-
+    <div className="container">
+      <Navbar />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+export default function Main() {
+    return (
+        <div>
+            <h1>Main Content goes here</h1>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+
+        </div>
+    )
+}
